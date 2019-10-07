b8fc1cbd28210053625b3487707a0fe7b4791eb4
Jun Gu <i@gujun.me>
2019-10-07 17:59:32 +0800

    AI-3.5 <jun@bogon Overwrite remote git@github.com:2dxgujun/Android-Studio-Settings.git to local

--- fileTemplates/code/JUnit4 Test Class.java
+++
@@ -1,5 +0,0 @@
-import static org.junit.Assert.*;
-#parse("My Header.java")
-public class ${NAME} {
-  ${BODY}
-}
--- fileTemplates/j2ee/Application.java
+++
@@ -1,7 +0,0 @@
-package ${PACKAGE_NAME};
-
-import android.app.Application;
-
-#parse("My Header.java")
-public class ${NAME} extends Application {
-}
